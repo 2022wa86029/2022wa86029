945f0be9446eb22d9451b149df8a0f061caac92e
Md Saquib Khan <2022wa86029@wilp.bits-pilani.ac.in>
2026-09-19 15:52:14 +0530

    Modify application source for CI test

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins Continuous Integration Lab");
+        System.out.println("Hello from Jenkins Continuous Integration Lab - Version 2");
     }
 }
